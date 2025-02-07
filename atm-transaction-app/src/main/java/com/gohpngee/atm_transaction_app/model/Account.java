35096c4db781f8a19ef6ff69ba895676f3074894
Amelia Lee <amelialee@Amelias-MacBook-Air.local>
2025-02-07 15:03:40 +0800

    added lombok for model layer, and using Builder

--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
@@ -1,8 +1,13 @@
 package com.gohpngee.atm_transaction_app.model;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Account {
     @Id
@@ -15,49 +20,4 @@ public class Account {
     private String accountHolderName;
     private String accountType;
     private BigDecimal balance;
-
-    public Account() {}
-
-    public Account(String accountNumber, String accountHolderName, String accountType, BigDecimal balance) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.accountType = accountType;
-        this.balance = balance;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public String getAccountNumber(){
-        return accountNumber;
-    }
-
-    public String getAccountHolderName(){
-        return accountHolderName;
-    }
-
-    public void setAccountHolderName(String accountHolderName){
-        this.accountHolderName = accountHolderName;
-    }
-
-    public String getAccountType(){
-        return accountType;
-    }
-
-    public void setAccountType(String accountType){
-        this.accountType = accountType;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void checkBalance() {
-        System.out.println("Balance: " + balance);
-    }
 }
