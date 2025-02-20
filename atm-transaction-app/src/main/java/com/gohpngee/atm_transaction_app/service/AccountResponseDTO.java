fc4c3883a88cbb97661c777ef801ef4957794485
Brian Goh <gohpngee@gmail.com>
2025-02-21 03:43:18 +0800

    added dependency for mysql and spring data jpa

--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/service/AccountResponseDTO.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/service/AccountResponseDTO.java
@@ -2,14 +2,13 @@ package com.gohpngee.atm_transaction_app.service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @RequiredArgsConstructor
 public class AccountDTO {
-    private final int accountNumber;
+    private final String accountNumber;
     private final BigDecimal balance;
 
 }
