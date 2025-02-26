c367e978c04008f5315b0820d0a7e7ed398ed195
Brian Goh <gohpngee@gmail.com>
2025-02-26 14:37:20 +0800

    added getters and setters, as well as constructors from lombok

--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
@@ -22,45 +22,4 @@ public class Account {
     private String accountType;
     @Column(nullable = false)
     private BigDecimal balance;
-
-    /*public Account(String accountNumber, String accountHolderName, String accountType, BigDecimal balance) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.accountType = accountType;
-        this.balance = balance;
-    }
-
-    public Account(){}*/
-
-    /*public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public void setAccountHolderName(String accountHolderName) {
-        this.accountHolderName = accountHolderName;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }*/
 }
