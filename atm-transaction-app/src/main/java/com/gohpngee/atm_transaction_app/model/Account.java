52b1f1b7b5ace4845b468a3c3517307d1f5b41ce
Amelia Lee <amelialee@Amelias-MacBook-Air.local>
2025-02-26 18:26:22 +0800

    added builder annotation

--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/model/Account.java
@@ -1,9 +1,6 @@
 package com.gohpngee.atm_transaction_app.model;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 import java.math.BigDecimal;
@@ -12,6 +9,7 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class Account {
     @Column(unique = true, nullable = false)
