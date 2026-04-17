ceac7db3c5ad7eb396f5a54c73ad31cb391a5d30
Brian Goh <briangoh@Brians-MacBook-Air.local>
2026-04-17 14:31:23 +0800

    Add keycloak configuration

---
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.gohpngee.atm_transaction_app.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+
+        return http.build();
+    }
+}
