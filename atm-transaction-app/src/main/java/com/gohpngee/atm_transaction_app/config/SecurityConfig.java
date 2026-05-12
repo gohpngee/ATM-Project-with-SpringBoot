bb551e268b2565125f026b2ea98083d990de5c18
Brian Goh <gohpngee@gmail.com>
2026-05-12 16:58:00 +0800

    Change ports for containers and healthcheck logic

--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/config/SecurityConfig.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.gohpngee.atm_transaction_app.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,12 +23,33 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/health").permitAll()
-                .requestMatchers("/api/*").authenticated()
-                .anyRequest().authenticated()
+                .authorizeHttpRequests(
+                    auth -> auth.anyRequest().authenticated()
+                )
+            .oauth2ResourceServer(outh2 -> outh2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Collection<GrantedAuthority> authorities = authoritiesConverter.convert(jwt);
+
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                List<String> roles = (List<String>) realmAccess.get("roles");
+                authorities.addAll(roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .toList());
+            }
+            return authorities;
+        });
+        return converter;
     }
 }
--- atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/controller/AccountController.java
+++ atm-transaction-app/src/main/java/com/gohpngee/atm_transaction_app/controller/AccountController.java
@@ -1,15 +1,21 @@
 package com.gohpngee.atm_transaction_app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.gohpngee.atm_transaction_app.dto.CreateAccountDTO;
 import com.gohpngee.atm_transaction_app.dto.DepositWithdrawDTO;
 import com.gohpngee.atm_transaction_app.dto.ShowBalanceDTO;
 import com.gohpngee.atm_transaction_app.dto.TransferDTO;
 import com.gohpngee.atm_transaction_app.model.Account;
 import com.gohpngee.atm_transaction_app.service.AccountService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -25,30 +31,35 @@ public class AccountController {
         return "Welcome to the home page";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<String> createAccount(@RequestBody CreateAccountDTO dto) {
         accountService.createAccount(dto);
         return ResponseEntity.ok("Account " + dto.getAccountHolderName() + " Created Successfully!");
     }
 
+    @PreAuthorize("hasRole('USER', 'ADMIN')")
     @PutMapping("/deposit")
     public ResponseEntity<String> deposit(@RequestBody DepositWithdrawDTO dto) {
         accountService.deposit(dto);
         return ResponseEntity.ok("Deposit of " + dto.getAmount() + " is successful for account " + dto.getAccountNumber());
     }
 
+    @PreAuthorize("hasRole('USER', 'ADMIN')")
     @PutMapping("/withdraw")
     public ResponseEntity<String> withdraw(@RequestBody DepositWithdrawDTO dto) {
         accountService.withdraw(dto);
         return ResponseEntity.ok( "Withdraw of " + dto.getAmount() + " is successful for account " + dto.getAccountNumber());
     }
 
+    @PreAuthorize("hasRole('USER', 'ADMIN')")
     @PutMapping("/transfer")
     public ResponseEntity<String> transfer(@RequestBody TransferDTO dto) {
         accountService.transfer(dto);
         return ResponseEntity.ok("Transfer of $" + dto.getAmount() + " is successful from account " + dto.getSenderAccountNumber() + " to account " + dto.getReceiverAccountNumber());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/balance/{accountNumber}")
     public ResponseEntity<String> showBalance(@PathVariable String accountNumber) {
         ShowBalanceDTO dto = new ShowBalanceDTO(accountNumber, null, null);
